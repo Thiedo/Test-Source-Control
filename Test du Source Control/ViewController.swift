678f65057e2a132aae634c0e252a2cb5bebdf260
Marame <Marame@mac-mini-de-marame.local>
2018-09-11 01:57:05 +0100

    Second commit

--- Test du Source Control/ViewController.swift
+++ Test du Source Control/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("premier commit")
+        print("deuxieme commit")
     }
 
     override func didReceiveMemoryWarning() {
