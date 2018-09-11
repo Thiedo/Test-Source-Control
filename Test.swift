aef50c7fc000fbd945202003cd5dc7adcac7db99
Marame <Marame@mac-mini-de-marame.local>
2018-09-11 02:11:56 +0100

    Ajout classe test

---
+++ Test.swift
@@ -0,0 +1,15 @@
+//
+//  Test.swift
+//  Test du Source Control
+//
+//  Created by Marame on 11/09/2018.
+//  Copyright © 2018 Marame. All rights reserved.
+//
+
+import Foundation
+
+class Test {
+    func salut() {
+        print("salut les gars")
+    }
+}
